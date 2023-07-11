5b715881becf381a2cd77482ff0db57606e19321
KaziMuktadirAhmed <kazi.muktadir@brainstation-23.com>
2023-07-11 21:04:41 +0600

    adds tailwind configuaration

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import { Routes, Route } from "react-router-dom";
+import Layout from "./components/layout/layout";
+
 import Home from "./pages/home/home";
 import Auth from "./pages/auth/auth";
 import Dashboard from "./pages/dashboard/dashboard";
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />}></Route>
-      <Route path="/auth" element={<Auth />}></Route>
-      <Route path="/dashboard" element={<Dashboard />}></Route>
-    </Routes>
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/auth" element={<Auth />}></Route>
+        <Route path="/dashboard" element={<Dashboard />}></Route>
+      </Routes>
+    </Layout>
   );
 }
