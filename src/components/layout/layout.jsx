9f108cd06feab36a76c372db53a44a36f06746b6
KaziMuktadirAhmed <kazi.muktadir@brainstation-23.com>
2023-07-12 12:41:29 +0600

    adds hello world and goodbye world workflow

--- src/components/layout/layout.jsx
+++ src/components/layout/layout.jsx
@@ -3,10 +3,10 @@ import Footer from "../footer/footer";
 
 export default function Layout({ children }) {
   return (
-    <>
+    <div className="flex flex-col justify-between min-h-screen">
       <Header />
       {children}
       <Footer />
-    </>
+    </div>
   );
 }
